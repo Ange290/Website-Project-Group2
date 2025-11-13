140d8e5d1d5f0daaeac0da82adfda11503319a1a
Regis Mucyo <mucyoregis250@gmail.com>
2025-11-13 20:09:14 +0200

    feat:Add position of menu contents and responsiviness

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const closeMenu = document.querySelector(".close-menu");
 const menuLists = document.querySelector(".menuList");
 const toggle = () => {
   [menu, closeMenu].forEach((element) => element.classList.toggle("hidden"));
-  // menuLists.classList.toggle("hidden");
+  menuLists.classList.toggle("hidden");
 };
 menu.addEventListener("click", toggle);
 closeMenu.addEventListener("click", toggle);
