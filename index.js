99f1ed2e687499fa249c99f5fb4933704e5ea2db
Regis Mucyo <mucyoregis250@gmail.com>
2025-11-13 18:24:39 +0200

    feat:Add contents in menu

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close-menu");
+const menuLists = document.querySelector(".menuList");
 const toggle = () => {
   [menu, closeMenu].forEach((element) => element.classList.toggle("hidden"));
+  // menuLists.classList.toggle("hidden");
 };
 menu.addEventListener("click", toggle);
 closeMenu.addEventListener("click", toggle);
