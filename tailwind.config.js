59d97cbb754d8f5da7d2aad83ae064f4df95ea14
Kubanabaraka <byukusengekubanabarakapascal@gmail.com>
2025-11-17 19:52:52 +0200

    chore: apply  tailwind config with custom sizing and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
+  content: [
     "./index.html",
     "./src//*.{html,js}",
     "./src/sections/**/*.{html,js}",
@@ -11,7 +11,24 @@ export default {
       fontFamily: {
         outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        0.9: "0.938rem",
+        2: "2rem",
+        10: "10rem",
+        6.5: "6.5rem",
+        17: "17rem",
+      },
+      colors: {
+        secondary: "#00642d",
+      },
+      height: {
+        1.4: "1.438rem",
+        2.3: "2.344rem",
+      },
+      width: {
+        3.9: "3.97rem",
+      },
     },
   },
   plugins: [],
-}
+};
